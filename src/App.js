6d89ddc21fd540285d9d34cec728fa94f55808ff
Melissa Lopez <melissalopez@pursuit.org>
2022-08-04 18:17:38 -0400

    just learned about setInterval and clearinterval

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
 import styled from "styled-components";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 //props
 const birdSize = 20;
 const gameHeight = 500;
 const gameWidth = 500;
+const gravity = 3;
 
 function App() {
+//position of bird will be at 250px
+const [birdPosition, setBirdPosition] = useState(250)
+
+//top position = 0;
+// bottom position = 480px;
+
+//The setInterval() function is commonly used to set a delay for functions that are executed again and again, such as animations.
+
+//clearInterval() method cancels a timed, repeating action which was previously established by a call to setInterval()
+
+
+useEffect(() => {
+  let timeId;
+if(birdPosition < gameHeight - birdSize){
+  timeId = setInterval(() => {
+  setBirdPosition(birdPosition => birdPosition + gravity)
+}, 24); //execute this line of code after a 24ms delay continuously.
+}
+
+return () => {
+  clearInterval(timeId)
+};
+});
+
+  
   return (
     <Div>
       <GameBox height={gameHeight} width={gameWidth}>
-      <Bird size={birdSize} />
+      <Bird size={birdSize} top={birdPosition} />
       </GameBox>
     </Div>
   );
 }
 
-export default App;
 
+export default App;
 
 const Bird = styled.div`
   position: absolute;
   background-color: red;
   height: ${(props) => props.size}px;
   width: ${(props) => props.size}px;
-  top: ${(props) => props.size}px;
+  top: ${(props) => props.top}px;
   border-radius: 50%;
   `;
 
