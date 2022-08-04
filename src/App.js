16d75b4576a75cf4e5d76d303b8545a870d8d962
Melissa Lopez <melissalopez@pursuit.org>
2022-08-04 15:28:36 -0400

    installed npm and added some css'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import styled from "styled-components";
+
+const birdSize = 20;
+
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
+    <Div>
+      <Bird size = {birdSize} />
+    </Div>
   );
 }
 
 export default App;
+
+
+const Bird = styled.div`
+  position: absolute;
+  background-color: red;
+  height: ${(props) => props.size}px;
+  width: ${(props) => props.size}px;
+  top: ${(props) => props.size}px;
+  border-radius: 50%;
+  `;
+
+  const Div = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    `;
